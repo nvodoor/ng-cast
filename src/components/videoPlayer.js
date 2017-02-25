f3596cefad39c92bc8d49dd47d1cd52e9eba128e
nvodoor <nvodoor@gmail.com>
2017-02-24 20:37:31 -0800

    Final commits for the day.

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,8 +1,11 @@
 angular.module('video-player')
 
 .directive('videoPlayer', function() {
+  var videoString = 'https://www.youtube.com/embed/1w8Z0UOXVaY';
   return {
-    // TODO
+    scope: {
+      videoListForPlayer: '<',
+    },
     templateUrl: 'src/templates/videoPlayer.html'
   };
 });
